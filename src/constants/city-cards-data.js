4ca245d666482d86bbdc7c461f0dd1d787e04059
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-07-05 21:39:25 -0300

    updated photos

--- src/constants/city-cards-data.js
+++ src/constants/city-cards-data.js
@@ -4,21 +4,21 @@ export const cityCardsData = [
         text: "¡Siento cómo mi corazón late más rápido al imaginar mi aventura en la Ciudad del Amor! Caminar por las calles empedradas, saborear croissants deliciosos y admirar la majestuosa Torre Eiffel sería un sueño hecho realidad. ¡Déjame volar hacia la hermosa París!",
         imageURL: "https://photos.smugmug.com/photos/i-nRT4QsP/0/X4/i-nRT4QsP-X4.jpg"
     },
-    { 
-        title: "Descubriendo Montañas Místicas",
-        text: "Cierro los ojos y me imagino en una emocionante expedición hacia las montañas más místicas del mundo. Ser testigo de paisajes impresionantes, respirar el aire puro de la naturaleza y perderme en las majestuosas cumbres sería una experiencia inolvidable. ¡Sueño con explorar esas montañas!",
-        imageURL: "https://photos.smugmug.com/photos/i-gMzWk4v/0/X4/i-gMzWk4v-X4.jpg"
-    },
+    // { 
+    //     title: "Descubriendo Montañas Místicas",
+    //     text: "Cierro los ojos y me imagino en una emocionante expedición hacia las montañas más místicas del mundo. Ser testigo de paisajes impresionantes, respirar el aire puro de la naturaleza y perderme en las majestuosas cumbres sería una experiencia inolvidable. ¡Sueño con explorar esas montañas!",
+    //     imageURL: "https://photos.smugmug.com/photos/i-gMzWk4v/0/X4/i-gMzWk4v-X4.jpg"
+    // },
     {
         title:"Sumergida en Playas del Paraíso",
         text:"Anhelo la tranquilidad de las playas paradisíacas con su arena blanca y aguas cristalinas. Sentir el sol cálido en mi piel, sumergirme en el océano turquesa y disfrutar de la brisa tropical sería un regalo maravilloso. ¡Sueño con descubrir el paraíso en esas playas exóticas!",
         imageURL:"https://photos.smugmug.com/photos/i-DpgmZDk/0/X5/i-DpgmZDk-X5.jpg"
     },
-    {              
-        title: "Encanto Oriental",
-        text: "Mi imaginación vuela hacia un mundo de misterio y encanto oriental. Explorar antiguos templos, maravillarme con la arquitectura milenaria y sumergirme en la vibrante cultura sería una experiencia única. ¡Sueño con dejarme llevar por los aromas y sabores exóticos de ese viaje!",
-        imageURL: "https://photos.smugmug.com/photos/i-VdX36dT/0/X5/i-VdX36dT-X5.jpg"
-    },
+    // {              
+    //     title: "Encanto Oriental",
+    //     text: "Mi imaginación vuela hacia un mundo de misterio y encanto oriental. Explorar antiguos templos, maravillarme con la arquitectura milenaria y sumergirme en la vibrante cultura sería una experiencia única. ¡Sueño con dejarme llevar por los aromas y sabores exóticos de ese viaje!",
+    //     imageURL: "https://photos.smugmug.com/photos/i-VdX36dT/0/X5/i-VdX36dT-X5.jpg"
+    // },
     {
         title: "Aventuras Salvajes en la Selva",
         text: "Mi corazón se llena de emoción al pensar en adentrarme en la selva y descubrir su fauna y flora increíbles. Explorar senderos ocultos, escuchar los sonidos de la naturaleza y maravillarme con la diversidad de especies sería un regalo asombroso. ¡Sueño con vivir una experiencia salvaje en la selva!",
@@ -32,11 +32,11 @@ export const cityCardsData = [
     {
         title: "Explorando la Ciudad de los Rascacielos",
         text: "Me imagino paseando por las calles vibrantes de una metrópolis moderna y llena de vida. Admiro los rascacielos imponentes y me sumerjo en una mezcla de culturas y sabores. ¡Sueño con explorar la ciudad que nunca duerme y vivir una experiencia urbana inolvidable!",
-        imageURL: "https://photos.smugmug.com/photos/i-BnVZQcr/0/X5/i-BnVZQcr-X5.jpg"
-    },
-    {
-        title: "Conectándome con la Naturaleza",
-        text: "Anhelo escapar del ajetreo de la ciudad y sumergirme en paisajes naturales de ensueño. Explorar bosques encantados, maravillarme con cascadas serenas y caminar por senderos llenos de vida sería un regalo especial para mi cumpleaños. ¡Sueño con conectarme con la naturaleza en mi día especial!",
         imageURL: "https://photos.smugmug.com/photos/i-XvwjGHR/0/X5/i-XvwjGHR-X5.jpg"
-    }          
+    },
+    // {
+    //     title: "Conectándome con la Naturaleza",
+    //     text: "Anhelo escapar del ajetreo de la ciudad y sumergirme en paisajes naturales de ensueño. Explorar bosques encantados, maravillarme con cascadas serenas y caminar por senderos llenos de vida sería un regalo especial para mi cumpleaños. ¡Sueño con conectarme con la naturaleza en mi día especial!",
+    //     imageURL: "https://photos.smugmug.com/photos/i-XvwjGHR/0/X5/i-XvwjGHR-X5.jpg"
+    // }          
 ];
